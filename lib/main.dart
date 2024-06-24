c873fd63551cebcaf888eb6568fe2dd838341b6f
ilzamafif <ilzamafif27@gmail.com>
2024-06-24 10:46:10 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter WebView Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WebView Example"),
+      ),
+      body: WebView(
+        initialUrl: 'https://wyasaaplikasi.com',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
