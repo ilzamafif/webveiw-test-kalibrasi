88fef318e2808421ea162661918e12d1f769f09b
ilzamafif <ilzamafif27@gmail.com>
2024-07-17 09:58:31 +0700

    Fix Camera Scan

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,109 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-void main() async {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Permission.camera.request();
-  await Permission.microphone
-      .request(); // if you need microphone permissionasync
-  runApp(MyApp());
-}
+  if (!kIsWeb &&
+      kDebugMode &&
+      defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+  runApp(const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MyApp()
+      )
     );
-  }
 }
 
-Future<void> requestCameraPermission() async {
-  var status = await Permission.camera.status;
-  if (!status.isGranted) {
-    await Permission.camera.request();
-  }
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: WebView(
-          initialUrl: 'https://kalibrasi.wyasaaplikasi.com',
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-          webViewController.evaluateJavascript(
-              "navigator.mediaDevices.getUserMedia({ video: true })");
-        },
-      )),
-    );
+        appBar: AppBar(
+          actions: [
+            TextButton(
+                onPressed: () async {
+                  await webViewController?.loadUrl(
+                      urlRequest: URLRequest(
+                          url: WebUri("https://apprtc.webrtcserver.cn/")));
+                },
+                child: const Text(
+                  "AppRTC",
+                  style: TextStyle(color: Colors.white),
+                )),
+            TextButton(
+                onPressed: () async {
+                  await webViewController?.loadUrl(
+                      urlRequest: URLRequest(
+                          url: WebUri(
+                              "https://www.pubnub.com/developers/demos/webrtc/launch/")));
+                },
+                child: const Text("PubNub WebRTC",
+                    style: TextStyle(color: Colors.white)))
+          ],
+        ),
+        body: Column(children: <Widget>[
+          Expanded(
+            child: InAppWebView(
+              key: webViewKey,
+              initialUrlRequest:
+                  URLRequest(url: WebUri("https://kalibrasi.wyasaaplikasi.com/")),
+              initialSettings: InAppWebViewSettings(
+                mediaPlaybackRequiresUserGesture: false,
+                allowsInlineMediaPlayback: true,
+              ),
+              onWebViewCreated: (controller) {
+                webViewController = controller;
+              },
+              onPermissionRequest: (controller, request) async {
+                final resources = <PermissionResourceType>[];
+                if (request.resources.contains(PermissionResourceType.CAMERA)) {
+                  final cameraStatus = await Permission.camera.request();
+                  if (!cameraStatus.isDenied) {
+                    resources.add(PermissionResourceType.CAMERA);
+                  }
+                }
+                if (request.resources
+                    .contains(PermissionResourceType.MICROPHONE)) {
+                  final microphoneStatus =
+                      await Permission.microphone.request();
+                  if (!microphoneStatus.isDenied) {
+                    resources.add(PermissionResourceType.MICROPHONE);
+                  }
+                }
+                // only for iOS and macOS
+                if (request.resources
+                    .contains(PermissionResourceType.CAMERA_AND_MICROPHONE)) {
+                  final cameraStatus = await Permission.camera.request();
+                  final microphoneStatus =
+                      await Permission.microphone.request();
+                  if (!cameraStatus.isDenied && !microphoneStatus.isDenied) {
+                    resources.add(PermissionResourceType.CAMERA_AND_MICROPHONE);
+                  }
+                }
+
+                return PermissionResponse(
+                    resources: resources,
+                    action: resources.isEmpty
+                        ? PermissionResponseAction.DENY
+                        : PermissionResponseAction.GRANT);
+              },
+            ),
+          ),
+        ]));
   }
 }
