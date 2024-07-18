5b72c7ad220424a5a2b46638afd47f28a5385f74
ilzamafif <ilzamafif27@gmail.com>
2024-07-18 09:04:09 +0700

    trial login goolge

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:googleapis/drive/v3.dart' as drive;
+import 'package:googleapis_auth/auth_io.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,11 +14,7 @@ Future main() async {
     await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
   }
 
-  runApp(const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyApp()
-      )
-    );
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -27,83 +26,97 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final GlobalKey webViewKey = GlobalKey();
+  static const _clientId =
+      '812976116770-7q6pr2ebj481dk1t5necb64d510n7ot3.apps.googleusercontent.com';
+  static const _scopes = ['https://www.googleapis.com/auth/drive.file'];
+  late AuthClient _client;
 
   InAppWebViewController? webViewController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: [
-            TextButton(
-                onPressed: () async {
-                  await webViewController?.loadUrl(
-                      urlRequest: URLRequest(
-                          url: WebUri("https://apprtc.webrtcserver.cn/")));
-                },
-                child: const Text(
-                  "AppRTC",
-                  style: TextStyle(color: Colors.white),
-                )),
-            TextButton(
-                onPressed: () async {
-                  await webViewController?.loadUrl(
-                      urlRequest: URLRequest(
-                          url: WebUri(
-                              "https://www.pubnub.com/developers/demos/webrtc/launch/")));
-                },
-                child: const Text("PubNub WebRTC",
-                    style: TextStyle(color: Colors.white)))
-          ],
-        ),
         body: Column(children: <Widget>[
-          Expanded(
-            child: InAppWebView(
-              key: webViewKey,
-              initialUrlRequest:
-                  URLRequest(url: WebUri("https://kalibrasi.wyasaaplikasi.com/")),
-              initialSettings: InAppWebViewSettings(
-                mediaPlaybackRequiresUserGesture: false,
-                allowsInlineMediaPlayback: true,
-              ),
-              onWebViewCreated: (controller) {
-                webViewController = controller;
-              },
-              onPermissionRequest: (controller, request) async {
-                final resources = <PermissionResourceType>[];
-                if (request.resources.contains(PermissionResourceType.CAMERA)) {
-                  final cameraStatus = await Permission.camera.request();
-                  if (!cameraStatus.isDenied) {
-                    resources.add(PermissionResourceType.CAMERA);
-                  }
-                }
-                if (request.resources
-                    .contains(PermissionResourceType.MICROPHONE)) {
-                  final microphoneStatus =
-                      await Permission.microphone.request();
-                  if (!microphoneStatus.isDenied) {
-                    resources.add(PermissionResourceType.MICROPHONE);
-                  }
-                }
-                // only for iOS and macOS
-                if (request.resources
-                    .contains(PermissionResourceType.CAMERA_AND_MICROPHONE)) {
-                  final cameraStatus = await Permission.camera.request();
-                  final microphoneStatus =
-                      await Permission.microphone.request();
-                  if (!cameraStatus.isDenied && !microphoneStatus.isDenied) {
-                    resources.add(PermissionResourceType.CAMERA_AND_MICROPHONE);
-                  }
-                }
-
-                return PermissionResponse(
-                    resources: resources,
-                    action: resources.isEmpty
-                        ? PermissionResponseAction.DENY
-                        : PermissionResponseAction.GRANT);
-              },
-            ),
+      Expanded(
+        child: InAppWebView(
+          key: webViewKey,
+          initialUrlRequest: URLRequest(url: WebUri("iot.wyasaaplikasi.com")),
+          initialSettings: InAppWebViewSettings(
+            mediaPlaybackRequiresUserGesture: false,
+            allowsInlineMediaPlayback: true,
           ),
-        ]));
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+          },
+          onPermissionRequest: (controller, request) async {
+            final resources = <PermissionResourceType>[];
+            if (request.resources.contains(PermissionResourceType.CAMERA)) {
+              final cameraStatus = await Permission.camera.request();
+              if (!cameraStatus.isDenied) {
+                resources.add(PermissionResourceType.CAMERA);
+              }
+            }
+            if (request.resources.contains(PermissionResourceType.MICROPHONE)) {
+              final microphoneStatus = await Permission.microphone.request();
+              if (!microphoneStatus.isDenied) {
+                resources.add(PermissionResourceType.MICROPHONE);
+              }
+            }
+            // only for iOS and macOS
+            if (request.resources
+                .contains(PermissionResourceType.CAMERA_AND_MICROPHONE)) {
+              final cameraStatus = await Permission.camera.request();
+              final microphoneStatus = await Permission.microphone.request();
+              if (!cameraStatus.isDenied && !microphoneStatus.isDenied) {
+                resources.add(PermissionResourceType.CAMERA_AND_MICROPHONE);
+              }
+            }
+
+            return PermissionResponse(
+                resources: resources,
+                action: resources.isEmpty
+                    ? PermissionResponseAction.DENY
+                    : PermissionResponseAction.GRANT);
+          },
+        ),
+      ),
+      ElevatedButton(
+        onPressed: _handleAuthClick,
+        child: Text('Show Flutter homepage'),
+      ),
+    ]));
+  }
+
+  Future<void> _handleAuthClick() async {
+    final auth = await clientViaUserConsent(
+        ClientId(_clientId, null), _scopes, _launchUrl);
+    setState(() {
+      _client = auth;
+    });
+    await _uploadFile();
+  }
+
+  Future<void> _uploadFile() async {
+    final driveApi = drive.DriveApi(_client);
+    final file = drive.File();
+    file.name = 'data.txt';
+    file.mimeType = 'text/plain';
+
+    final fileContent = 'Hello, world!';
+    final media = drive.Media(
+      Stream.value(fileContent.codeUnits),
+      fileContent.length,
+    );
+
+    final response = await driveApi.files.create(file, uploadMedia: media);
+    print('File ID: ${response.id}');
+  }
+
+  Future<void> _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import flutter_inappwebview_macos
+import google_sign_in_ios
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
