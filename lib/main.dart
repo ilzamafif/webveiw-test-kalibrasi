424f08b9aabf748b754dedcc839540d53cb792c3
ilzamafif <ilzamafif27@gmail.com>
2024-07-19 11:38:59 +0700

    done v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'dart:async';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Title Your App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Container(
-          child: WebView(
-            initialUrl: 'https://iot.wyasaaplikasi.com/',
-            javascriptMode: JavascriptMode.unrestricted,
-            onWebViewCreated: (WebViewController webViewController) {
-              _controller.complete(webViewController);
-            },
-            navigationDelegate: (NavigationRequest request) {
-              if (request.url.startsWith("https://iot.wyasaaplikasi.com/")) {
-                return NavigationDecision.navigate;
-              } else {
-                _launchURL(request.url);
-                return NavigationDecision.prevent;
-              }
-            },
-          ),
-        ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+  final AudioPlayer audioPlayer = AudioPlayer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: 'https://iot.wyasaaplikasi.com/', // Ganti dengan URL awal Anda
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+        navigationDelegate: (NavigationRequest request) {
+          if (request.url.startsWith("https://iot.wyasaaplikasi.com/")) {
+            return NavigationDecision.navigate;
+          } else if (request.url.startsWith("myapp://redirect")) {
+            // Custom URL scheme to handle redirect back to the app
+            handleCustomUrlScheme(request.url);
+            return NavigationDecision.prevent;
+          } else {
+            _launchURL(request.url);
+            return NavigationDecision.prevent;
+          }
+        },
       ),
     );
   }
-
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
 
   _launchURL(String url) async {
     if (await canLaunch(url)) {
@@ -45,5 +61,44 @@ class MyApp extends StatelessWidget {
     } else {
       throw 'Could not launch $url';
     }
+  }
+
+  void handleCustomUrlScheme(String url) {
+    // Extract data from the URL if needed
+    Uri uri = Uri.parse(url);
+    String? token = uri.queryParameters['access_token'];
+
+    // Handle the redirect and navigate to a different screen if needed
+    if (token != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => SuccessPage(token: token)),
+      );
+    } else {
+      // Handle the case where the token is null
+      print('No access token found in URL');
+    }
+  }
+
+  void playAlarm() async {
+    await audioPlayer.play(AssetSource('assets/audio/alarm.mp3'));
+  }
+}
+
+class SuccessPage extends StatelessWidget {
+  final String token;
+
+  SuccessPage({required this.token});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Success"),
+      ),
+      body: Center(
+        child: Text("Data successfully uploaded! Token: $token"),
+      ),
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import flutter_inappwebview_macos
+import google_sign_in_ios
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
