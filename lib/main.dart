cc91a0031edcbb66a9b782dd27aebe3543fc6951
ilzamafif <ilzamafif27@gmail.com>
2024-07-19 13:23:00 +0700

    update: nama aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Title Your App',
+      title: 'Thermocouple',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
